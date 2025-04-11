8a3ed6d7c67d0e19de57a949ae22c3ea8854a4aa
Gourab <gourabdas2128@gmail.com>
2025-04-11 11:46:39 +0530

    updated for the error loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // ✅ Updated here
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Index from "./pages/Index";
 import Achievement from "./pages/Achievement";
@@ -23,7 +22,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <Router> {/* ✅ Updated here */}
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/achievement" element={<Achievement />} />
@@ -35,7 +34,7 @@ const App = () => (
             <Route path="/news/:id" element={<NewsDetail />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
