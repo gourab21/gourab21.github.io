f10c064e7f2b45e9b5e3567e24e9937f7b8ecc94
Gourab <gourabdas2128@gmail.com>
2025-04-15 15:12:42 +0530

    images adding

--- src/components/gallery/galleryData.ts
+++ src/components/gallery/galleryData.ts
@@ -9,39 +9,124 @@ export interface GalleryImage {
 // Sample gallery images
 export const galleryImages: GalleryImage[] = [
   {
-    id: "1",
-    title: "Photography",
-    description: "Beautiful mountain landscape with a clear blue sky",
-    src: "/lovable-uploads/gallery/riverclick.jpeg"
-  },
-  {
-    id: "2",
-    title: "10+2 School",
-    description: "The Senior Secondary School Building of Ramakrishna Mission Vidyapith, Deoghar",
-    src: "/lovable-uploads/gallery/plus2.jpeg"
-  },
-  {
-    id: "3",
+    id: "101",
     title: "Winner Turing Show 2025",
     description: "Tranquil ocean scene with waves crashing on shore",
     src: "/lovable-uploads/gallery/turingshow2025.jpg"
   },
+
   {
-    id: "4",
+    id: "102",
     title: "Winner Codeplay 2025",
     description: "Peaceful forest path surrounded by tall trees",
     src: "/lovable-uploads/gallery/codeplay2025.jpg"
   },
+
   {
-    id: "5",
-    title: "Ocean View",
-    description: "Tranquil ocean scene with waves crashing on shore",
+    id: "103",
+    title: "Thakur's Tithi Puja 2025",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
     src: "/lovable-uploads/gallery/thakurtithipuja2025.jpg"
   },
+
   {
-    id: "6",
-    title: "Forest Trail",
-    description: "Peaceful forest path surrounded by tall trees",
-    src: "/lovable-uploads/gallery/saraswati.jpg"
-  }
+    id: "103i",
+    title: "Kamakshya Hill",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/kamakshya_hill.jpg"
+  },
+
+  {
+    id: "103ii",
+    title: "Gandhi Mandap Guwahati",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/gandhi_mandap.jpg"
+  },
+
+  {
+    id: "10333ivi",
+    title: "Rashtrapati Bhavan",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/rashtrapati_1.jpg"
+  },
+
+  {
+    id: "10333ii",
+    title: "India gate",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/india_gate.jpg"
+  },
+  {
+    id: "10333iii",
+    title: "ISKCON Temple Delhi",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/iskcon_delhi.jpg"
+  },
+
+  {
+    id: "10333",
+    title: "Graduation College",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/arsd_me.jpg"
+  },
+
+  {
+    id: "10333i",
+    title: "Pixelation Society",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/pixelation_group.jpg"
+  },
+
+  {
+    id: "10333a",
+    title: "ARSD Library",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/arsd_library.jpg"
+  },
+
+  {
+    id: "10333b",
+    title: "ARSD Computer Lab",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/arsd_comp_lab.jpg"
+  },
+
+  {
+    id: "10333c",
+    title: "ARSD Campus",
+    description: "Bhagwan Sri Ramakrishna's Tithi Puja Celebration 2025, We carried his charriot to the temple at Belur Math",
+    src: "/lovable-uploads/gallery/arsd.jpg"
+  },
+
+
+  {
+    id: "1033",
+    title: "Reunion at Deoghar 2023",
+    description: "Met my friends after a long time at Deoghar, Indranil Mahanty (left) & Dev Sinha (right)",
+    src: "/lovable-uploads/gallery/deoghar_three.jpg"
+  },
+
+  {
+    id: "104",
+    title: "10+2 School",
+    description: "The Senior Secondary School Building of Ramakrishna Mission Vidyapith, Deoghar",
+    src: "/lovable-uploads/gallery/plus2.jpeg"
+  },
+
+  {
+    id: "105i",
+    title: "Capturing the Moment",
+    description: "Beautiful mountain landscape with a clear blue sky",
+    src: "/lovable-uploads/gallery/photography.jpg"
+  },
+
+  {
+    id: "105",
+    title: "Photography",
+    description: "Beautiful mountain landscape with a clear blue sky",
+    src: "/lovable-uploads/gallery/riverclick.jpeg"
+  },
+
+
+  
 ];
